9ca3b46c83c66697fe8b90f4b3f4d50f45f07d57
Jakub Walerstein <jakub.d.w@gmail.com>
2020-02-12 01:30:54 -0500

    added loop folder structure, new loops, and loop notes

--- htmlgen.sh
+++ htmlgen.sh
@@ -2,14 +2,14 @@
 
 URL="<div style=\"audioblock\">
         <audio loop controls preload="none">
-          <source src=\"https://github.com/jakubwalerstein/jakubwalerstein.github.io/blob/master/loops/FILENAME?raw=true\" type=\"audio/wav\">
+          <source src=\"https://github.com/jakubwalerstein/jakubwalerstein.github.io/blob/master/loops/synth/FILENAME?raw=true\" type=\"audio/wav\">
           Your browser does not support the audio element.
         </audio>
         FILENAME
       </div>
 "
 
-for file in loops/*
+for file in loops/synth/*
 do  
 	echo "${URL//FILENAME/$(basename "$file")}"  >> loophtml.txt
 done
